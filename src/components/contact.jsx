e845a2b7ac2ba41039acfa974e2240dc7eb4a91c
meghachavan2705 <64421460+meghachavan2705@users.noreply.github.com>
2022-11-12 19:15:37 +0530

    contact details change contact page

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -148,12 +148,7 @@ export const Contact = (props) => {
       </div>
       <div id="footer">
         <div className="container text-center">
-          <p>
-            &copy; 2020 Issaaf Kattan React Land Page Template. Design by{" "}
-            <a href="http://www.templatewire.com" rel="nofollow">
-              TemplateWire
-            </a>
-          </p>
+          <p>&copy; Towards the extraordinary dreams of ordinary people</p>
         </div>
       </div>
     </div>
